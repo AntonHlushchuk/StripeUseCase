a7ff9666761807e17680b9e80931d3818a4a6811
Anton Hlushchuk <anton.hlushchuk@wework.com>
2023-08-02 10:26:45 +0300

    UC#2 Create Stripe card element

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: '*',
     methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Content-Type, Authorization'
 }));
